869087fda0ec55d37c991b38926762cf39049c6a
Bruce Li <brucelee@1-6deMacBook-Pro.local>
2025-10-15 20:38:55 -0500

    mp3: ready for render deploy

--- database_scripts/dbClean.py
+++ database_scripts/dbClean.py
@@ -32,20 +32,18 @@ def getUsers(conn):
     return users
 
 def getTasks(conn):
-    # Retrieve the list of tasks
+
     conn.request("GET","""/api/tasks?filter={"_id":1}""")
     response = conn.getresponse()
     data = response.read()
     d = json.loads(data)
 
-    # Array of user IDs
     tasks = [str(d['data'][x]['_id']) for x in range(len(d['data']))]
 
     return tasks
 
 def main(argv):
 
-    # Server Base URL and port
     baseurl = "localhost"
     port = 4000
 
@@ -63,40 +61,30 @@ def main(argv):
         elif opt in ("-p", "--port"):
              port = int(arg)
 
-    # Server to connect to (1: url, 2: port number)
     conn = http.client.HTTPConnection(baseurl, port)
 
-    # Fetch a list of users
     users = getUsers(conn)
 
-    # Loop for as long as the database still returns users
     while len(users):
 
-        # Delete each individual user
         for user in users:
             conn.request("DELETE","/api/users/"+user)
             response = conn.getresponse()
             data = response.read()
 
-        # Fetch a list of users
         users = getUsers(conn)
 
-    # Fetch a list of tasks
     tasks = getTasks(conn)
 
-    # Loop for as long as the database still returns tasks
     while len(tasks):
 
-        # Delete each individual task
         for task in tasks:
             conn.request("DELETE","/api/tasks/"+task)
             response = conn.getresponse()
             data = response.read()
 
-        # Fetch a list of tasks
         tasks = getTasks(conn)
 
-    # Exit gracefully
     conn.close()
     print("All users and tasks removed at "+baseurl+":"+str(port))
 
